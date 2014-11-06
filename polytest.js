6bd31e8882bbd73b6a2676f90907ee4bb7b28220
Anthony Hinsinger <anthony.hinsinger@univ-pau.fr>
2014-11-06 21:31:38 +0100

    Started meteor-x elements

--- polytest.js
+++ polytest.js
@@ -1,31 +1,9 @@
 if (Meteor.isClient) {
-
-  Template.layout.events({
-    'click paper-button': function () {
-      document.querySelector('paper-dialog').toggle();
-    }
-  });
-
-  Template.layout.cards = function() {
-    return [
-      {
-        name: "Kelly Slater",
-        periode: "Periode 2",
-        reportent: "sdsqdsqdsqd",
-        reportiut: "dfdfsdfffffsdfsf",
-      },
-      {
-        name: "Jean-Luc Brassard",
-        periode: "Periode 3",
-        reportent: "sdsqdsqdsqd",
-        reportiut: "dfdfsdfffffsdfsf",
-      },
-    ];
-  };
-}
-
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
+  Meteor.startup(function() {
+    $('body')
+      .attr('unresolved', '')
+      .attr('fullbleed', '');
   });
 }
+
+if (Meteor.isServer) {}
