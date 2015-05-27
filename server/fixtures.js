23a26596a2d357880010564a49039594b7864491
Anthony Hinsinger <anthony.hinsinger@univ-pau.fr>
2015-05-27 17:09:31 +0200

    Added toast

--- server/fixtures.js
+++ server/fixtures.js
@@ -11,28 +11,8 @@ if (Music.find({}).count() < 1) {
     Music.insert({
       artist: artist,
       album: faker.lorem.words().slice(0,2).join(" "),
-      arturl: faker.image.image()+"/"+album.replace(" ", "%20"),
+      arturl: faker.image.image()+"/"+album.replace(/\s/g, "%20"),
       tracks: tracks
     });
   }
-
-
-  /*Music.insert({
-    artist: "Daft Punk",
-    album: "Discovery",
-    art: "#D81B60",
-    tracks: ["One more time", "Aerodynamic", "Digital Love", "Harder, Better, Faster, Stronger"]
-  });
-  Music.insert({
-    artist: "Fisherspooner",
-    album: "#1",
-    art: "#00BCD4",
-    tracks: ["Invisible", "The 15th", "Emerge", "Fucker", "Turn On"]
-  });
-  Music.insert({
-    artist: "Rone",
-    album: "Bora EP",
-    art: "#C6FF00",
-    tracks: ["Bora", "Bora Vocal", "Flesh", "Spanish breakfast", "Dry"]
-  });*/
 }
